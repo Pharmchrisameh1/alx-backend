d6ee33078282e71b56fc5088a9d58f9fb324ff0b
Rx Chris Ameh <chris1jnr@gmail.com>
2025-05-20 16:59:17 -0700

    updated files

--- 0x02-i18n/0-app.py
+++ 0x02-i18n/0-app.py
@@ -1,19 +1,16 @@
 #!/usr/bin/env python3
-"""A Basic Flask app.
 """
+Basic Flask app
+"""
+
 from flask import Flask, render_template
 
-
 app = Flask(__name__)
-app.url_map.strict_slashes = False
 
 
-@app.route('/')
-def get_index() -> str:
-    """The home/index page.
+@app.route('/', methods=['GET'], strict_slashes=False)
+def index():
+    """
+    hello world
     """
     return render_template('0-index.html')
-
-
-if __name__ == '__main__':
-    app.run(host='0.0.0.0', port=5000)
